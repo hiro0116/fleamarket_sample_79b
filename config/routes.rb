7d2056ee403cbd0c3c1c9dd3e3a471427581334b
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-07-28 16:02:34 +0900

    master

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'posts#show'
+
   resources :posts, only: :show
-  # root to: 'items#create'
+  root to: 'items#create'
   resources :items, only: [:create]
-  root to: 'posts#index'
+
 end

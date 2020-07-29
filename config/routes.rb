fc2262c6cc78964fa86b122f6e037dfe5fd2635b
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-07-29 18:08:25 +0900

    user_Mypage4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   resources :posts, only: :show
-  # root to: 'items#create'
   resources :items, only: [:create]
-  # root to: 'posts#index'
   resources :users, only: :index
 end
